88358dfb2cfab5dedc2abc5ae63d5710a7219258
Sahal Shaikh <shaikhsahal125@gmail.com>
2022-01-01 14:29:05 -0500

    "Starting Word Stack"

--- ScarnesDice/app/src/main/java/com/example/scarnesdice/MainActivity.java
+++ ScarnesDice/app/src/main/java/com/example/scarnesdice/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void computerTurn() {
-        System.out.println("Inside Computer Turen");
+        System.out.println("Inside Computer Turn");
 
         Handler handler = new Handler();
         Runnable runnable = new Runnable() {
